394dd0c098ef395ab4f33ed6a5c413b711577f30
Ademu Anthony <ademuanthony@gmail.com>
2020-05-23 18:30:57 +0100

    get

--- cmd/web-app/handlers/routes.go
+++ cmd/web-app/handlers/routes.go
@@ -295,7 +295,7 @@ func APP(shutdown chan os.Signal, appCtx *AppContext) http.Handler {
 		Renderer:        appCtx.Renderer,
 	}
 	app.Handle("POST", "/user/login", u.Login)
-	app.Handle("POST", "/login", u.Login)
+	app.Handle("GET", "/login", u.Login)
 	app.Handle("GET", "/user/login", u.Login, waitDbMid)
 	app.Handle("GET", "/user/logout", u.Logout)
 	app.Handle("POST", "/user/reset-password/:hash", u.ResetConfirm)
