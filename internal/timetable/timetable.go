60e4e4eb5b97d7ca3326c77904c604b3e491dcda
Ademu Anthony <ademuanthony@gmail.com>
2020-07-08 20:48:05 +0100

    Timetable order

--- internal/timetable/timetable.go
+++ internal/timetable/timetable.go
@@ -118,6 +118,8 @@ func (repo *Repository) StudentsTimetables(ctx context.Context, studentID string
 		qm.Load(models.TimetableRels.Subclass),
 		qm.Load(models.TimetableRels.Subject),
 		qm.Load(models.TimetableRels.Teacher),
+		qm.OrderBy(models.TimetableColumns.Day),
+		// qm.OrderBy(models.TimetableColumns.PeriodID),
 	).All(ctx, repo.DbConn)
 	if err != nil {
 		if err.Error() == sql.ErrNoRows.Error() {
